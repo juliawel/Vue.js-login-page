d4abb48f54878366c448f6ce4792bf0592fa4663
juliawel <julia20.wel@hotmail.com>
2024-10-07 11:27:47 -0300

    firebase

---
+++ src/firebase/index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCQRkn__tJ5NQOzHcK3eUdQQ7DiyvuWZe4",
+  authDomain: "vue-firebase-auth-c388f.firebaseapp.com",
+  projectId: "vue-firebase-auth-c388f",
+  storageBucket: "vue-firebase-auth-c388f.appspot.com",
+  messagingSenderId: "128168297952",
+  appId: "1:128168297952:web:24a5653a898dc116781785"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
