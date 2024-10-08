fd29350851b9afad67764c4aadcfb92e585f9a05
juliawel <julia20.wel@hotmail.com>
2024-10-08 10:16:56 -0300

    .

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,6 +1,16 @@
 <template>
     <div class="login-container">
         <h2>Login</h2>
+        <form @submit.prevent="loginUser">
+            <div>
+                <label for="email">Email</label>
+                <input type="email" v-model="email" required>
+            </div>
+            <div>
+                <label for="password">Password</label>
+                <input type="password" v-model="password" required>
+            </div>
+        </form>
     </div>
 </template>
 
