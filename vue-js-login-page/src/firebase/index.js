a3f2dd62805f77f1b53c4aeda44610bc5ff08618
juliawel <julia20.wel@hotmail.com>
2024-10-09 10:25:31 -0300

    firebase

---
+++ vue-js-login-page/src/firebase/index.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD8zK3RAGVIMFWMFqmDrr3xQQAX4WgBC2E",
+  authDomain: "vue-fire-auth-e3b40.firebaseapp.com",
+  projectId: "vue-fire-auth-e3b40",
+  storageBucket: "vue-fire-auth-e3b40.appspot.com",
+  messagingSenderId: "958158662525",
+  appId: "1:958158662525:web:757e30617fbeac63c2ba14"
+};
+
+const app = initializeApp(firebaseConfig);
