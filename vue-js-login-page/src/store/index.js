370fa133f5b1e651bfabc5bfab4a77d134edfd4d
juliawel <julia20.wel@hotmail.com>
2024-10-10 11:21:05 -0300

    .

--- vue-js-login-page/src/store/index.js
+++ vue-js-login-page/src/store/index.js
@@ -1,4 +1,10 @@
 import { createStore } from 'vuex'
+import router from '../router'
+import { auth } from '../firebase'
+import { createUserWithEmailAndPassword, 
+         signInWithEmailAndPassword,
+         signOut } from 'firebase/auth'
+import { createBlock } from 'vue'
 
 export default createStore({
   state: {
@@ -13,17 +19,64 @@ export default createStore({
     }
   },
   actions: {
-    /*
     async login ({ commit}, details) {
-      //
+      const { email, password } = details
+
+      try {
+        await signInWithEmailAndPassword(auth, email, password)
+      } catch(error) {
+        switch(error.code) {
+          case 'auth/user-not-found':
+            alert('User not found')
+            break
+          case 'auth/wrong-password':
+            alert('Wrong password')
+            break
+          default:
+            alert('Something went wrong')
+        }
+
+        return 
+      }
+
+      commit('SET_USER', auth.currentUser)
+
+      router.push('/')
     },
     async logout ({ commit}) {
       //
     },
     async register ({ commit}, details) {
-      //
+      const { email, password } = details
+
+      try {
+        await createUserWithEmailAndPassword(auth, email, password)
+      } catch(error) {
+        switch(error.code) {
+          case 'auth/email-already-in-use':
+            alert('Email already in use')
+            break
+          case 'auth/invalid-email':
+            alert('Invalid email')
+            break
+          case 'auth/oparation-not-allowed':
+            alert('Operation not allowed')
+            break
+          case 'auth/weak-password':
+            alert('Weak password')
+            break
+          default:
+            alert('Something went wrong')
+        }
+
+        return 
+      }
+
+      commit('SET_USER', auth.currentUser)
+
+      router.push('/')
     }
-    */
+    
   },
 
 })
